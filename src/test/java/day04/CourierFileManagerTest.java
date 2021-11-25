5dec4fce133d726d22dc0b2a222f65a2be53c39a
gdevecz <gdevecz@gmail.com>
2021-11-25 16:49:18 +0100

    day04 feladat majdnem kész

---
+++ src/test/java/day04/CourierFileManagerTest.java
@@ -0,0 +1,14 @@
+package day04;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourierFileManagerTest {
+
+    CourierFileManager cm =
+    @Test
+    void crateCourierByFile() {
+
+    }
+}
