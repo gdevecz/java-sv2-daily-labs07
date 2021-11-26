3eb37e8327abac60adb22dab97a80c2c69ec861c
gdevecz <gdevecz@gmail.com>
2021-11-26 03:43:04 +0100

    day04 Finished, formázás, nevek, elütések, file teszt megírva, a vizsgálat átnevezve és átírva, tesztek kiegészítve

--- src/main/java/day04/CourierMain.java
+++ src/main/java/day04/CourierMain.java
@@ -9,7 +9,7 @@ public class CourierMain {
         CourierFileManager cm = new CourierFileManager();
 
         Path path = Paths.get("src/main/resources/day04/rides.txt");
-        Courier courier= cm.crateCourierByFile(path);
+        Courier courier = cm.crateCourierByFile(path);
 
         System.out.println(courier.getRides());
         //courier.addRide(new Ride(1,1,1));
