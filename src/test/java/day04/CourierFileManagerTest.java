57bbb4fbd2a023526a75356f1a69eeb47c9ff4bc
gdevecz <gdevecz@gmail.com>
2021-11-26 03:42:12 +0100

    day04 Finished, formázás, nevek, elütések, file teszt megírva, a vizsgálat átnevezve és átírva, tesztek kiegészítve

--- src/test/java/day04/CourierFileManagerTest.java
+++ src/test/java/day04/CourierFileManagerTest.java
@@ -11,11 +11,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class CourierFileManagerTest {
 
     CourierFileManager cfm = new CourierFileManager();
+
     @Test
     void TestCrateCourierByFile() {
         Path path = Paths.get("src/main/resources/day04/rides.txt");
         Courier courier = cfm.crateCourierByFile(path);
-        Ride expectedRide = new Ride(4,1,19);
+        Ride expectedRide = new Ride(4, 1, 19);
 
         assertEquals(6, courier.getRides().size());
         assertEquals(expectedRide.toString(), courier.getRides().get(3).toString());
@@ -25,7 +26,7 @@ class CourierFileManagerTest {
     void TestCreateCourierByFileFailed() {
         Path path = Path.of("src/main/resources/day04/wrong.txt");
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-                ()-> cfm.crateCourierByFile(path));
+                () -> cfm.crateCourierByFile(path));
 
         assertEquals("Can not read file: " + path, exception.getMessage());
         assertEquals(NoSuchFileException.class, exception.getCause().getClass());
